7273a675335fa4a080349340dac76b8aeaa2c369
George Japploon Wilson <32513173+Japploon@users.noreply.github.com>
2019-05-28 15:27:29 +0100

    EdMum

---
+++ MayhemJam/Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 10;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+           
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+        }
+    }
+}
