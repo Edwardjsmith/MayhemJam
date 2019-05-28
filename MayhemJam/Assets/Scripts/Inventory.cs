f7c03db631aadc088bc98204c6042a55814dd6f7
Ed <dsa>
2019-05-28 16:02:44 +0100

    Started inventory

---
+++ MayhemJam/Assets/Scripts/Inventory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    const int bagSpace = 4;
+    Dictionary<string, Sprite> items;
+    Sprite[] currentItems = new Sprite[bagSpace];
+
+    Sprite defaultSprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        items = new Dictionary<string, Sprite>();
+
+        foreach (string file in Directory.GetFiles("Sprites"))
+        {
+            string itemName = Path.GetFileNameWithoutExtension(file);
+            items.Add(itemName, (Sprite)Resources.Load(file));
+
+            if (itemName == "default")
+            {
+                defaultSprite = items[itemName];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void addItem(string itemName)
+    {
+        for (int i = 0; i < currentItems.Length; i++)
+        {
+            if (currentItems[i] == defaultSprite)
+            {
+                currentItems[i] = items[itemName];
+            }
+        }
+    }
+
+    public void removeItem(int position)
+    {
+        currentItems[position] = defaultSprite;
+    }
+}
