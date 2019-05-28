7422500be0732fcd2fa5b5ea83d6554b8c386165
Falor <b-heroes@t-online.de>
2019-05-28 16:57:03 +0100

    Added some senses

---
+++ MayhemJam/Assets/Scripts/AI/HearingAIScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HearingAIScript : MonoBehaviour {
+
+    public List<Vector3> positions;
+
+    private void Update()
+    {
+        if (positions.Count > 3)
+            positions.RemoveAt(0);
+    }
+
+
+
+
+}
---
+++ MayhemJam/Assets/Scripts/AI/NoiseScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseScript : MonoBehaviour {
+
+    private SphereCollider collider;
+
+    private void Start()
+    {
+        collider = GetComponent<SphereCollider>();
+    }
+
+    void CreateNoise(float range)
+    {
+        GetComponent<SphereCollider>().radius = range;
+    }
+
+    private void Update()
+    {
+        if(collider.radius > 0)
+        {
+            collider.radius = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<HearingAIScript>() != null)
+            other.gameObject.GetComponent<HearingAIScript>().positions.Add(transform.position);
+    }
+
+
+
+
+}
