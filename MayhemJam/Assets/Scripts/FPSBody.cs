3684e817a97b16048476bb257f9e114626debb46
Ed <dsa>
2019-05-28 12:35:07 +0100

    Player script and decrepit dungeon added

---
+++ MayhemJam/Assets/Scripts/FPSBody.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class FPSBody : MonoBehaviour
+{
+    public delegate void SoundTrigger(Vector3 playerPos);
+    public static event SoundTrigger playerSound;
+
+    public float movementSpeed = 10;
+    CharacterController controller;
+
+    float openDistance = 5.0f;
+
+    public LayerMask rayLayer;
+
+	// Use this for initialization
+	public void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizInput = Input.GetAxis("Horizontal") * movementSpeed;
+        float vertInput = Input.GetAxis("Vertical") * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        controller.SimpleMove(forwardMovement + rightMovement);
+    }
+}
---
+++ MayhemJam/Assets/Scripts/FPSCam.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class FPSCam : MonoBehaviour
+{
+    float sensitivity = 100.0f;
+    float xAxisClamp = 0;
+    Transform playerBody;
+    bool bodyAttached = false;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    // Use this for initialization
+    public void Start()
+    {
+        if (transform.parent != null)
+        {
+            playerBody = transform.parent;
+            bodyAttached = true;
+        }
+    }
+
+    private void Update()
+    {
+        camRotation();
+    }
+
+    void camRotation()
+    {
+        float mouseX = -Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = -Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxis(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxis(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+
+        if (bodyAttached)
+        {
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+        else if(!bodyAttached)
+        {
+            transform.Rotate(Vector3.up * mouseX);
+        }
+    }
+
+    void ClampXAxis(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+
+
+}
